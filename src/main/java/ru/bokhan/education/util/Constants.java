705d552a15bf7ad9351d202bce93cf5fd45e11d1
ozrush <shyaksh@gmail.com>
2020-09-02 05:27:10 +0300

    Refactor UserTestData, add Constants

---
+++ src/main/java/ru/bokhan/education/util/Constants.java
@@ -0,0 +1,7 @@
+package ru.bokhan.education.util;
+
+public class Constants {
+    public static final int START_SEQ = 100000;
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+}
--- src/test/java/ru/bokhan/education/UserTestData.java
+++ src/test/java/ru/bokhan/education/UserTestData.java
@@ -6,13 +6,11 @@ import ru.bokhan.education.model.User;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.bokhan.education.util.Constants.ADMIN_ID;
+import static ru.bokhan.education.util.Constants.USER_ID;
 
 public class UserTestData {
-    public static final int START_SEQ = 100000;
     public static final int NOT_FOUND = 10;
-    public static final int USER_ID = START_SEQ;
-    public static final int ADMIN_ID = START_SEQ + 1;
-
     public static final User USER = new User(USER_ID, "User", "user@gmail.com", "Password", Role.USER, null);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "Password", Role.ADMIN, null);
 
@@ -32,9 +30,11 @@ public class UserTestData {
     public static void assertMatch(User actual, User expected) {
         assertThat(actual).isEqualToIgnoringGivenFields(expected, "role");
     }
+
     public static void assertMatch(Iterable<User> actual, User... expected) {
         assertMatch(actual, Arrays.asList(expected));
     }
+
     public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
         assertThat(actual).usingElementComparatorIgnoringFields("role").isEqualTo(expected);
     }
